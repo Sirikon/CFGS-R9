d342e62b9d513323404259dd122480a0255b393e
Carlos Fernández <sirikon2@gmail.com>
2015-03-25 14:12:16 +0100

    Empezando ejercicio 2

---
+++ R9E2/R9E2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace R9E2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            float res = TextCalc.Calc("2+2-7*4/6");
+            MessageBox.Show(res.ToString());
+        }
+    }
+}
---
+++ R9E2/R9E2/TextCalc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R9E2
+{
+    class TextCalc
+    {
+        class ExpresionMalFormadaException : Exception {}
+
+        public static float Calc (String text)
+        {
+            List<Object> elements = new List<Object>();
+            for (int i = 0; i < text.Length; i++ )
+            {
+                Char c = text[i];
+                if(!Char.IsNumber(c) && c != '+' && c == '-' && c == '*' && c == '/')
+                {
+                    throw new ExpresionMalFormadaException();
+                }
+            }
+
+            // Merge the numbers
+            for (int i = 0; i < text.Length; i++ )
+            {
+
+            }
+
+            return elements.Count;
+        }
+    }
+}
